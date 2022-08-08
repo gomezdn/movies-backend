0e57ef1becf787b81ac83cf2b16c8a7bcf2475b8
gomezdn <gomezdn@outlook.com>
2022-08-08 04:26:12 -0300

    created pool for db connection

--- src/database/database.js
+++ src/database/database.js
@@ -7,6 +7,11 @@ const { DB_DBNAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
 const db = new Sequelize(DB_DBNAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
+  pool: {
+    max: 30,
+    acquire: 10000,
+    idle: 3000,
+  },
 });
 
 module.exports = { db };
