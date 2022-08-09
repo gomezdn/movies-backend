f707d32a5e5bbef330d4341e35dd7a41b7c0a64a
gomezdn <gomezdn@outlook.com>
2022-08-09 01:46:45 -0300

    email service

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -4,7 +4,6 @@ const transporter = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
   port: process.env.NODEMAILER_PORT,
   secure: false,
-  greetingTimeout: 60000,
   auth: {
     user: process.env.NODEMAILER_REMITENT,
     pass: process.env.NODEMAILER_PASSWORD,
